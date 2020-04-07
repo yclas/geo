c8b495f5bd28747e69de47d371cd13eb940f849d
Oliver <oliver@open-classifieds.com>
2020-04-07 15:42:31 -0500

    Fixes column mismatch

--- countries/BM-Bermuda.sql
+++ countries/BM-Bermuda.sql
@@ -1,12 +1,12 @@
-INSERT INTO `oc2_locations` (`id_location`, `name`, `order`, `id_location_parent`, `parent_deep`, `seoname`, `description`, `last_modified`, `has_image`, `latitude`, `longitude`, `id_geoname`, `fcodename_geoname`, `translation_name`, `translations`)
+INSERT INTO `oc2_locations` (`id_location`, `name`, `order`, `id_location_parent`, `parent_deep`, `seoname`, `description`, `last_modified`, `has_image`, `latitude`, `longitude`, `id_geoname`, `fcodename_geoname`, `translations`)
 VALUES
-	(198976,'Devonshire',0,1,0,'devonshire-742271',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(198977,'Hamilton',0,1,0,'hamilton-742272',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(198978,'Paget',0,1,0,'paget-742273',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(198979,'Pembroke',0,1,0,'pembroke-742274',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(198980,'Saint',0,1,0,'saint-742275',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(198981,'Saint George\'s',0,1,0,'saint-georges-742276',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(198982,'Sandys',0,1,0,'sandys-742277',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(198983,'Smith\'s',0,1,0,'smiths-742278',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(198984,'Southampton',0,1,0,'southampton-742279',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(198985,'Warwick',0,1,0,'warwick-742280',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL);
+	(198976,'Devonshire',0,1,0,'devonshire-742271',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(198977,'Hamilton',0,1,0,'hamilton-742272',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(198978,'Paget',0,1,0,'paget-742273',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(198979,'Pembroke',0,1,0,'pembroke-742274',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(198980,'Saint',0,1,0,'saint-742275',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(198981,'Saint George\'s',0,1,0,'saint-georges-742276',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(198982,'Sandys',0,1,0,'sandys-742277',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(198983,'Smith\'s',0,1,0,'smiths-742278',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(198984,'Southampton',0,1,0,'southampton-742279',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(198985,'Warwick',0,1,0,'warwick-742280',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL);
--- countries/SM-San-Marino.sql
+++ countries/SM-San-Marino.sql
@@ -1,11 +1,11 @@
-INSERT INTO `oc2_locations` (`id_location`, `name`, `order`, `id_location_parent`, `parent_deep`, `seoname`, `description`, `last_modified`, `has_image`, `latitude`, `longitude`, `id_geoname`, `fcodename_geoname`, `translation_name`, `translations`)
+INSERT INTO `oc2_locations` (`id_location`, `name`, `order`, `id_location_parent`, `parent_deep`, `seoname`, `description`, `last_modified`, `has_image`, `latitude`, `longitude`, `id_geoname`, `fcodename_geoname`, `translations`)
 VALUES
-	(543139,'Acquaviva',0,1,0,'acquaviva-779898',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(543140,'Borgo Maggiore',0,1,0,'borgo-maggiore-779899',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(543141,'Chiesanuova',0,1,0,'chiesanuova-779900',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(543142,'Montegiardino',0,1,0,'montegiardino-779901',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(543143,'Domagnano',0,1,0,'domagnano-779902',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(543144,'Faetano',0,1,0,'faetano-779903',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(543145,'Fiorentino',0,1,0,'fiorentino-779904',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(543146,'San Marino',0,1,0,'san-marino-779905',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
-	(543147,'Serravalle',0,1,0,'serravalle-779906',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL);
+	(543139,'Acquaviva',0,1,0,'acquaviva-779898',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(543140,'Borgo Maggiore',0,1,0,'borgo-maggiore-779899',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(543141,'Chiesanuova',0,1,0,'chiesanuova-779900',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(543142,'Montegiardino',0,1,0,'montegiardino-779901',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(543143,'Domagnano',0,1,0,'domagnano-779902',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(543144,'Faetano',0,1,0,'faetano-779903',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(543145,'Fiorentino',0,1,0,'fiorentino-779904',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(543146,'San Marino',0,1,0,'san-marino-779905',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL),
+	(543147,'Serravalle',0,1,0,'serravalle-779906',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL);
