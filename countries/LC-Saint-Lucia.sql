838a04e99c496399958c36bc85189f0243df8db7
Oliver <oliver@open-classifieds.com>
2020-04-07 09:32:17 -0500

    SQL files

---
+++ countries/LC-Saint-Lucia.sql
@@ -0,0 +1,13 @@
+INSERT INTO `oc2_locations` (`id_location`, `name`, `order`, `id_location_parent`, `parent_deep`, `seoname`, `description`, `last_modified`, `has_image`, `latitude`, `longitude`, `id_geoname`, `fcodename_geoname`, `translation_name`, `translations`)
+VALUES
+	(427349,'Castries',0,1,0,'castries-779886',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(427350,'Choiseul',0,1,0,'choiseul-779887',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(427351,'Dauphin',0,1,0,'dauphin-779888',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(427352,'Dennery',0,1,0,'dennery-779889',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(427353,'Gros Islet',0,1,0,'gros-islet-779890',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(427354,'Laborie',0,1,0,'laborie-779891',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(427355,'Micoud',0,1,0,'micoud-779892',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(427356,'Praslin',0,1,0,'praslin-779893',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(427357,'Soufriere',0,1,0,'soufriere-779894',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(427358,'Vieux Fort',0,1,0,'vieux-fort-779895',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(427359,'Anse la Raye',0,1,0,'anse-la-raye-779896',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL);
---
+++ countries/PS-Palestinian-Territory.sql
@@ -0,0 +1,8 @@
+INSERT INTO `oc2_locations` (`id_location`, `name`, `order`, `id_location_parent`, `parent_deep`, `seoname`, `description`, `last_modified`, `has_image`, `latitude`, `longitude`, `id_geoname`, `fcodename_geoname`, `translation_name`, `translations`)
+VALUES
+	(524739,'محافظة القدس',0,1,0,'%d9%85%d8%ad%d8%a7%d9%81%d8%b8%d8%a9-%d8%a7%d9%84%d9%82%d8%af%d8%b3-782965',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(524740,'ابو دِيس',0,524739,0,'%d8%a7%d8%a8%d9%88-%d8%af%d9%90%d9%8a%d8%b3-515242',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(524741,'الجُدَيْرَة',0,524739,0,'%d8%a7%d9%84%d8%ac%d9%8f%d8%af%d9%8e%d9%8a%d9%92%d8%b1%d9%8e%d8%a9-515243',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(524742,'الجِيْب',0,524739,0,'%d8%a7%d9%84%d8%ac%d9%90%d9%8a%d9%92%d8%a8-515244',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(524743,'الرَام وضَاحِيَة البَريد',0,524739,0,'%d8%a7%d9%84%d8%b1%d9%8e%d8%a7%d9%85-%d9%88%d8%b6%d9%8e%d8%a7%d8%ad%d9%90%d9%8a%d9%8e%d8%a9-%d8%a7%d9%84%d8%a8%d9%8e%d8%b1%d9%8a%d8%af-515245',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL),
+	(524744,'الزْعَيِّمْ',0,524739,0,'%d8%a7%d9%84%d8%b2%d9%92%d8%b9%d9%8e%d9%8a%d9%91%d9%90%d9%85%d9%92-515246',NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL);
